68a98b3725bdcdec795c48e80b4b8f0bbbdecb6e
Fernando Celmer <email@fernandocelmer.com>
2026-04-07 02:23:25 -0300

    🪲 BUG: Fix post-gen hook name collision when project and module names are equal

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,18 +2,25 @@
 
 import os
 import shutil
+import tempfile
 from pathlib import Path
 
 project_dir = Path(os.getcwd())
 parent_dir = project_dir.parent
 
-for item in project_dir.iterdir():
-    dest = parent_dir / item.name
-    if dest.exists():
-        if dest.is_dir():
-            shutil.rmtree(dest)
-        else:
-            dest.unlink()
-    shutil.move(str(item), str(parent_dir))
+with tempfile.TemporaryDirectory() as tmp:
+    tmp_dir = Path(tmp)
 
-project_dir.rmdir()
+    for item in project_dir.iterdir():
+        shutil.move(str(item), str(tmp_dir / item.name))
+
+    project_dir.rmdir()
+
+    for item in tmp_dir.iterdir():
+        dest = parent_dir / item.name
+        if dest.exists():
+            if dest.is_dir():
+                shutil.rmtree(dest)
+            else:
+                dest.unlink()
+        shutil.move(str(item), str(dest))
